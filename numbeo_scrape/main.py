a7a997db27c24ced69dd16e56d1ed4b71e0a68b6
AsheshShrestha7 <ashesh.shrestha@utexas.edu>
2022-05-05 12:35:34 -0500

    create numbeo_scrape package and scrape quality of life index from numbeo

---
+++ numbeo_scrape/main.py
@@ -0,0 +1,19 @@
+### Importing the required libraries
+import requests
+from bs4 import BeautifulSoup
+from .scrape_quality_of_life_data import scrape_quality_of_life_index
+
+BASE_DIR = "csv_data"
+quality_of_life_index_csv_path = os.path.join(BASE_DIR, "quality_of_life_index_{}.csv")
+
+os.makedirs(BASE_DIR, exist_ok=True)
+
+url = "https://www.numbeo.com/quality-of-life/rankings.jsp"
+
+## Scraping data from multiple pages of the URL
+
+# The Year of the most recent data
+data = requests.get(url).text
+soup = BeautifulSoup(data, 'html.parser')
+year_latest = soup.find("form", class_="changePageForm").find("option").text
+scrape_quality_of_life_index(url, year_latest, quality_of_life_index_csv_path)
